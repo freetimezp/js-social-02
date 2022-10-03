40895a966ca87076291aef25cf973f56ac62ee1d
freetimezp <kraftukrnet@ukr.net>
2022-10-03 19:26:35 +0300

    07. js-social-02. Avatar component.

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {useSession} from "next-auth/react";
+import Avatar from "./Avatar";
 
 const PostBox = () => {
     const {data: session} = useSession();
 
     return (
-        <form>
+        <form className="sticky top-16 z-50 bg-white border rounded-md border-gray-300 p-2">
             <div className="flex items-center space-x-3">
+                <Avatar />
+
                 <input
                     type="text" disabled={!session}
                     className="bg-gray-50 p-2 pl-5 outline-none rounded-md flex-1"
