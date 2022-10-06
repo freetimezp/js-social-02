5fe80c9d330ec12ad3f440c64a7076f05806e4f8
freetimezp <kraftukrnet@ukr.net>
2022-10-06 23:49:10 +0300

    19. js-social-02. Cors... 07.

--- apollo-client.js
+++ apollo-client.js
@@ -4,10 +4,12 @@ const client = new ApolloClient({
     uri: "https://fangshan.stepzen.net/api/modest-salamander/__graphql",
     headers: {
         Authorisation: `ApiKey ${process.env.NEXT_PUBLIC_STEPZEN_KEY}`,
-        "key": "access-control-allow-origin",
-        "value": "*"
+        Accept: 'application/json',
+        method: "OPTIONS",
+        mode: 'cors',
     },
     cache: new InMemoryCache(),
 });
 
 export default client;
+
